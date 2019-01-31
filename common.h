642b3457fb89c50854777384d42809ff7df04cc5
Max Kimlyk <maxskater3@yandex.ru>
2019-01-31 23:26:24 +0300

    Demo macro

---
+++ common.h
@@ -0,0 +1,45 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+static std::vector<struct __demo_base__*> __demos__;
+
+struct __demo_base__
+{
+    virtual void operator()() const = 0;
+    std::string __name__;
+
+    __demo_base__(const std::string&& name) : __name__(name)
+    {
+        __demos__.push_back(this);
+    }
+
+};
+
+#define DEMO(name) \
+    struct name : public __demo_base__    \
+    {                                     \
+        name() : __demo_base__(#name) {}  \
+                                          \
+        void operator()() const override; \
+    } __##name##_inst__;                  \
+                                          \
+    void name::operator()() const
+
+#define RUN_DEMOS \
+    int main()                                                     \
+    {                                                              \
+        for (auto demo : __demos__)                                \
+        {                                                          \
+            std::cout << "===== " << demo->__name__ << " =====\n"; \
+            (*demo)();                                             \
+            std::cout << "\n";                                     \
+        }                                                          \
+        return 0;                                                  \
+    }
+
+#endif
