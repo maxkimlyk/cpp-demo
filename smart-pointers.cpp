182742fa7ae8a5367baf7db291a7860316e118c2
Max Kimlyk <maxskater3@yandex.ru>
2019-02-03 20:39:33 +0300

    enable_shared_from_this

--- smart-pointers.cpp
+++ smart-pointers.cpp
@@ -50,6 +50,9 @@ DEMO(shared_ptr_demo)
     auto sp2 = std::make_shared<SomeClass>(/* constr args */);
 
     auto copy = sp2; // copying is allowed
+
+    // Use count
+    std::cout << "sp2.use_count() = " << sp2.use_count() << "\n";
 }
 
 DEMO(weak_ptr_demo)
@@ -60,7 +63,7 @@ DEMO(weak_ptr_demo)
     std::shared_ptr<SomeClass> sp = std::make_shared<SomeClass>();
 
     // Creating
-    std::weak_ptr<SomeClass> wp(sp);// link with shared_ptr sp
+    std::weak_ptr<SomeClass> wp(sp); // link with shared_ptr sp
 
     // Access
     if (auto spt = wp.lock()) // lock() creates shared_ptr for access
@@ -79,6 +82,55 @@ DEMO(weak_ptr_demo)
     {
         std::cout << "Catched " << e.what() << "\n";
     }
+}
+
+DEMO(enable_shared_from_this)
+{
+    // Derivating from std::enable_shared_from_this allow by a shared_ptr to an object
+    // to safely generate additional shared_ptr's.
+
+    struct Good : std::enable_shared_from_this<Good>
+    {
+        std::shared_ptr<Good> get_ptr()
+        {
+            return shared_from_this();
+        }
+    };
+
+    struct Bad
+    {
+        std::shared_ptr<Bad> get_ptr()
+        {
+            return std::shared_ptr<Bad>(this);
+        }
+
+        ~Bad()
+        {
+            std::cout << "call ~Bad()\n";
+        }
+    };
+
+    // Two shared_ptr's share the same object
+    std::shared_ptr<Good> gp1 = std::make_shared<Good>();
+    std::shared_ptr<Good> gp2 = gp1->get_ptr();
+    std::cout << "gp2.use_count() = " << gp2.use_count() << "\n";
+
+    // But shared_from_this() is only for creating new shared_ptr from existing one
+    try
+    {
+        Good not_so_good;
+        std::shared_ptr<Good> gp = not_so_good.get_ptr(); // call shared_from_this()
+    }
+    catch (std::bad_weak_ptr& e)
+    {
+        std::cout << "Exception: " << e.what() << "\n";
+    }
+
+    // Each shared_ptr thinks that it is only one owner of the object
+    std::shared_ptr<Bad> bp1 = std::make_shared<Bad>();
+    std::shared_ptr<Bad> bp2 = bp1->get_ptr();
+    std::cout << "bp2.use_count() = " << bp2.use_count() << "\n";
+    // so we have double deletion of Bad.
 }
 
 RUN_DEMOS
