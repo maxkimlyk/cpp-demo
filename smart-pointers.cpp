f334d2dbc23eeb825831fb6411f372f90447fbb1
Max Kimlyk <maxskater3@yandex.ru>
2019-02-03 19:38:35 +0300

    weak_ptr

--- smart-pointers.cpp
+++ smart-pointers.cpp
@@ -16,11 +16,11 @@ struct SomeClass
 
 DEMO(unique_ptr_demo)
 {
-    // creating
+    // Creating
     std::unique_ptr<SomeClass> up1(new SomeClass(/* constr args */));
     auto up2 = std::make_unique<SomeClass>(/* constr args */);
 
-    // access
+    // Access
     up1->method();
     (*up1).method();
 
@@ -28,28 +28,57 @@ DEMO(unique_ptr_demo)
     auto moved = std::move(up1); // OK
     moved->method();
 
-    // get usual pointer
+    // Get usual pointer
     SomeClass* ptr = up2.get();
 
-    // array
+    // Array
     std::unique_ptr<SomeClass[]> up_to_array(new SomeClass[2]);
 
-    // custom deleter
+    // Custom deleter
     auto custom_delete_func = [](int*) { std::cout << "custom_delete_func\n"; };
     std::unique_ptr<int, std::function<void(int*)>> up3(new int(1), custom_delete_func);
 
-    // function return unique_ptr
+    // Function return unique_ptr
     auto func = []() { return std::make_unique<int>(10); };
     auto up4 = func(); // moving here
 }
 
 DEMO(shared_ptr_demo)
 {
-    // creating
+    // Creating
     std::shared_ptr<SomeClass> sp1(new SomeClass(/* constr args */));
     auto sp2 = std::make_shared<SomeClass>(/* constr args */);
 
     auto copy = sp2; // copying is allowed
+}
+
+DEMO(weak_ptr_demo)
+{
+    // std::weak_ptr models temporary ownership: when an object needs to be accessed
+    // only if it exists, and it may be deleted at any time by someone else.
+
+    std::shared_ptr<SomeClass> sp = std::make_shared<SomeClass>();
+
+    // Creating
+    std::weak_ptr<SomeClass> wp(sp);// link with shared_ptr sp
+
+    // Access
+    if (auto spt = wp.lock()) // lock() creates shared_ptr for access
+        spt->method();
+    else
+        std::cout << "wp is expired\n";
+
+    sp.reset(); // delete shared_ptr
+
+    // Access throwing exception
+    try
+    {
+        std::shared_ptr<SomeClass> spt(wp); // get shared_ptr from weak_ptr
+    }
+    catch (const std::bad_weak_ptr& e)
+    {
+        std::cout << "Catched " << e.what() << "\n";
+    }
 }
 
 RUN_DEMOS
