d5aca69fa68fc560f22892b6f5dfb8d52d9e6deb
Max Kimlyk <maxskater3@yandex.ru>
2019-02-12 21:07:37 +0300

    optional

---
+++ optional.cpp
@@ -0,0 +1,26 @@
+#include "common.h"
+
+#include <optional>
+
+std::optional<int> func(bool b)
+{
+    return b ? std::optional<int>(10) : std::nullopt;
+}
+
+DEMO(optional)
+{
+    auto var1 = func(false);
+    auto var2 = func(true);
+
+    if (var1) // operator bool overloaded
+        std::cout << "var1 has value "
+                  << *var1 // * returns containing object. Behavior is undefined when no object contained.
+                  << "\n";
+
+    if (var2.has_value())
+        std::cout << "var2 has value " << var2.value() << "\n";
+
+    std::cout << "var1 has value " << var1.value_or(-1) << " (-1 if empty)\n";
+}
+
+RUN_DEMOS
