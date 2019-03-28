b8cb7bcaeb7972152660c877f7fdd4ace81b82c4
Max Kimlyk <u131095@edu.miet.ru>
2019-03-28 09:22:25 +0300

    lambda-overload

---
+++ lambda-overload.cpp
@@ -0,0 +1,24 @@
+#include "common.h"
+
+// Inherit from each of passed lambdas.
+template<class... Ts>
+struct overloaded : Ts...
+{
+    using Ts::operator()...;
+};
+
+// Allow deduce template parameters of class from constructor.
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+int main()
+ {
+    const auto lambda = overloaded {
+        [](int i)   { std::cout << "int: " << i << "\n"; },
+        [](char ch) { std::cout << "char: " << ch << "\n"; }
+    };
+
+    lambda(1);
+    lambda('a');
+
+    return 0;
+}
