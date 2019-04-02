9318ead17235543b5fae22f09d8e441361ba223c
Max Kimlyk <u131095@edu.miet.ru>
2019-04-02 17:19:52 +0300

    more chrono

--- chrono.cpp
+++ chrono.cpp
@@ -3,7 +3,7 @@
 #include <chrono>
 #include <thread>
 
-DEMO(chrono)
+DEMO(time_measurement)
 {
     auto start = std::chrono::high_resolution_clock::now();
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -11,6 +11,18 @@ DEMO(chrono)
 
     std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count()
               << " seconds passed\n";
+
+    std::cout << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - start).count()
+              << " milliseconds passed\n";
+}
+
+DEMO(time_arithmetics)
+{
+    auto t1 = std::chrono::system_clock::now();
+    auto t2 = t1 + std::chrono::hours{1} + std::chrono::minutes{2};
+
+    std::cout << std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count()
+              << " seconds bettween two timestamps\n";
 }
 
 RUN_DEMOS
