2031856d15a5026aa2b682d92630a2f8328074d8
Max Kimlyk <u131095@edu.miet.ru>
2019-05-13 17:46:21 +0300

    multithread: promise, packaged_task, async

--- multithread.cpp
+++ multithread.cpp
@@ -1,8 +1,9 @@
 #include "demo-common.h"
 
-#include <thread>
+#include <future>
 #include <mutex>
 #include <shared_mutex>
+#include <thread>
 
 DEMO(mutex)
 {
@@ -42,7 +43,7 @@ DEMO(unique_lock)
     std::lock(lock2, lock3); // блокирует оба лока
 }
 
-DEMO(shared_mutex)
+/*DEMO(shared_mutex)
 {
     // std::shared_mutex поддерживает два режима блокировки
     // * для чтения - через std::shared_lock или метод lock_shared
@@ -67,7 +68,7 @@ DEMO(shared_mutex)
         {
             std::shared_lock locK(mutex);
             std::cout << "current count: " << count << "\n";
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     };
 
@@ -76,11 +77,68 @@ DEMO(shared_mutex)
 
     thread1.join();
     thread2.join();
-}
+}*/
 
 DEMO(sleep)
 {
     std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+DEMO(promise)
+{
+    // std::promise хранит объект (или исключение), который в дальнейшем может быть
+    // ассинхронно затребован через std::future.
+
+    const auto worker = [](std::promise<int> promise) {
+        int result = 1 + 2;
+        promise.set_value(result);
+    };
+
+    std::promise<int> promise;
+    std::future<int> future = promise.get_future();
+
+    std::thread thread(worker, std::move(promise));
+    thread.detach();
+
+    std::cout << "Future contains: " << future.get() << "\n";
+}
+
+DEMO(packaged_task)
+{
+    // std::packaged_task хранит функцию и позволяет получить доступ к ее результату через std::future.
+
+    const auto func = []() {
+        int count = 0;
+        for (size_t i = 1; i <= 4; ++i)
+            count += i;
+        return count;
+    };
+
+    std::packaged_task<int(void)> task(func);
+
+    std::future<int> future = task.get_future();
+
+    task(); // целесообразно выполнять в другом потоке
+
+    std::cout << "task result: " << future.get() << "\n";
+}
+
+DEMO(async)
+{
+    // std::async запускает функцию асинхронно (или сохраняет функцию для ленивого вычисления)
+    // и возвращает std::future, через который можно получить результат вызова функции.
+
+    const auto func = [](int a) {
+        return a + a;
+    };
+
+    std::future<int> future1 = std::async(std::launch::async, func, 1); // запускает func в отдельном потоке
+    std::future<int> future2 = std::async(std::launch::deferred, func, 2); // запустит func, когда будет востребован ее результат
+    std::future<int> future3 = std::async(func, 3); // может быть использована любая политика запуска (эквивалентно std::launch::async | std::launch::deferred)
+
+    std::cout << "future1: " << future1.get() << "\n";
+    std::cout << "future2: " << future2.get() << "\n";
+    std::cout << "future3: " << future3.get() << "\n";
 }
 
 RUN_DEMOS
