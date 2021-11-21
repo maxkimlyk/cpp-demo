76bc6ee7b37537704503c9f8e68d824cb0c7a304
Max Kimlyk <maxskater3@yandex.ru>
2021-11-21 08:57:29 +0300

    flat

--- boost-container.cpp
+++ boost-container.cpp
@@ -3,6 +3,7 @@
 #include <iterator>
 
 #include <boost/container/stable_vector.hpp>
+#include <boost/container/flat_set.hpp>
 
 DEMO(stable_vector)
 {
@@ -38,6 +39,25 @@ DEMO(stable_vector)
 
     std::cout << "iterator is still valid, rest of values: ";
     std::transform(iter, st_vec.end(), std::ostream_iterator<int>(std::cout, ", "), [](const auto& value) { return value.payload; });
+    std::cout << std::endl;
+}
+
+DEMO(flat_set_map) {
+  // flat_set, flat_map, flat_multiset, flat_multimap use vector
+  // of sorted values instead of tree for storing data.
+  // * due to spacial contiguity of data, we get
+  //   - faster iterators
+  //   - random access iterators
+  //   - faster search operations
+  //   - less memory consumption
+  // * slower insertion and erasure (according to vector)
+
+  boost::container::flat_set<int> set = {1, 5, 9, 2, 4};
+  set.insert(3);
+  set.insert(5);
+
+  std::cout << "set: ";
+  std::copy(set.begin(), set.end(), std::ostream_iterator<int>(std::cout, ", "));
 }
 
 RUN_DEMOS
