b3511555adbe06e4fec79dd68e2e1285a69670f0
Max Kimlyk <maxskater3@yandex.ru>
2021-11-23 18:26:40 +0300

    circular buffer

---
+++ boost-circular-buffer.cpp
@@ -0,0 +1,33 @@
+#include "demo-common.h"
+
+#include <iterator>
+
+#include <boost/circular_buffer.hpp>
+
+DEMO(circular_buffer)
+{
+    constexpr size_t capacity = 4;
+    boost::circular_buffer<int> buffer(capacity);
+
+    const auto print_buffer = [&buffer]() {
+        std::cout << "buffer: ";
+        std::copy(buffer.begin(), buffer.end(), std::ostream_iterator<int>(std::cout, ", "));
+        std::cout << std::endl;
+    };
+
+    std::cout << "pushing 5 times" << std::endl;
+    for (int i = 1; i <= 5; ++i)
+    {
+        buffer.push_back(i);
+    }
+
+    print_buffer();
+
+    std::cout << "pop front and back" << std::endl;
+    buffer.pop_front();
+    buffer.pop_back();
+
+    print_buffer();
+}
+
+RUN_DEMOS
