80371f5efbf6a993ab5640d1e275f7a7f03fde23
Max Kimlyk <maxskater3@yandex.ru>
2021-11-23 07:57:37 +0300

    static_vector

--- boost-container.cpp
+++ boost-container.cpp
@@ -5,6 +5,7 @@
 #include <boost/container/stable_vector.hpp>
 #include <boost/container/flat_set.hpp>
 #include <boost/container/slist.hpp>
+#include <boost/container/static_vector.hpp>
 
 DEMO(stable_vector)
 {
@@ -43,40 +44,66 @@ DEMO(stable_vector)
     std::cout << std::endl;
 }
 
-DEMO(flat_set_map) {
-  // flat_set, flat_map, flat_multiset, flat_multimap use vector
-  // of sorted values instead of tree for storing data.
-  // * due to spacial contiguity of data, we get
-  //   - faster iterators
-  //   - random access iterators
-  //   - faster search operations
-  //   - less memory consumption
-  // * slower insertion and erasure (according to vector)
+DEMO(flat_set_map)
+{
+    // flat_set, flat_map, flat_multiset, flat_multimap use vector
+    // of sorted values instead of tree for storing data.
+    // * due to spacial contiguity of data, we get
+    //   - faster iterators
+    //   - random access iterators
+    //   - faster search operations
+    //   - less memory consumption
+    // * slower insertion and erasure (according to vector)
 
-  boost::container::flat_set<int> set = {1, 5, 9, 2, 4};
-  set.insert(3);
-  set.insert(5);
+    boost::container::flat_set<int> set = {1, 5, 9, 2, 4};
+    set.insert(3);
+    set.insert(5);
 
-  std::cout << "set: ";
-  std::copy(set.begin(), set.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << "set: ";
+    std::copy(set.begin(), set.end(), std::ostream_iterator<int>(std::cout, ", "));
 }
 
-DEMO(slist) {
-  // slist is a singly linked list. Slist is a bit different than std::forward_list.
-  // * std::forward_list doesn't have size() method but allows
-  //   merging different lists via splice after in constant time
-  // * slist tracks its size
+DEMO(slist)
+{
+    // slist is a singly linked list. Slist is a bit different than std::forward_list.
+    // * std::forward_list doesn't have size() method but allows
+    //   merging different lists via splice after in constant time
+    // * slist tracks its size
 
-  boost::container::slist<int> list = {1, 2, 3, 4, 5};
+    boost::container::slist<int> list = {1, 2, 3, 4, 5};
 
-  std::cout << "list.size(): " << list.size(); // O(1)
-  std::cout << std::endl;
+    std::cout << "list.size(): " << list.size(); // O(1)
+    std::cout << std::endl;
 
-  boost::container::slist<int> another_list = {9, 9, 9};
-  list.splice_after(list.begin(), another_list); // O(size(another_list))
+    boost::container::slist<int> another_list = {9, 9, 9};
+    list.splice_after(list.begin(), another_list); // O(size(another_list))
 
-  std::cout << "list: ";
-  std::copy(list.begin(), list.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << "list: ";
+    std::copy(list.begin(), list.end(), std::ostream_iterator<int>(std::cout, ", "));
+}
+
+DEMO(static_vector)
+{
+    // static_vector has fixed size like array
+    // but doesn't require elements to be initialized when it is creating.
+
+    struct wo_def_ctor
+    {
+        wo_def_ctor(int v) : value(v)
+        {
+        }
+
+        int value;
+    };
+
+    boost::container::static_vector<wo_def_ctor, 10> static_v;
+    static_v.emplace_back(1);
+    static_v.emplace_back(2);
+    static_v.emplace_back(3);
+    static_v.pop_back();
+
+    std::cout << "static_v: ";
+    std::transform(static_v.begin(), static_v.end(), std::ostream_iterator<int>(std::cout, ", "), [](const auto obj) { return obj.v;});
 }
 
 RUN_DEMOS
