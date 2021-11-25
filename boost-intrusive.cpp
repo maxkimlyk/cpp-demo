9afe51978c11c75faad00be82eb152bd92309e4f
Max Kimlyk <maxskater3@yandex.ru>
2021-11-25 20:46:45 +0300

    intrusive set

--- boost-intrusive.cpp
+++ boost-intrusive.cpp
@@ -3,6 +3,7 @@
 #include <iterator>
 
 #include <boost/intrusive/list.hpp>
+#include <boost/intrusive/unordered_set.hpp>
 
 // Difference between intrusive and non-intrusive containers:
 // - non-intrusive containers store copies of values
@@ -41,7 +42,9 @@ DEMO(list)
         boost::intrusive::list_member_hook<> hook_;
         int value;
 
-        Bar(int value): value(value) {}
+        Bar(int value) : value(value)
+        {
+        }
     };
 
     Bar bar_1(1);
@@ -58,6 +61,68 @@ DEMO(list)
     std::cout << "Bar list: ";
     std::transform(bar_list.begin(), bar_list.end(), std::ostream_iterator<int>(std::cout, ","), [](const Bar& bar) { return bar.value; });
     std::cout << std::endl;
+}
+
+struct Foo : public boost::intrusive::unordered_set_base_hook<>
+{
+    int value;
+
+    Foo(int value) : value(value)
+    {
+    }
+
+    friend bool operator==(const Foo& a, const Foo& b)
+    {
+        return a.value == b.value;
+    }
+
+    friend std::size_t hash_value(const Foo& value)
+    {
+        return std::size_t(value.value);
+    }
+};
+
+DEMO(unordered_set)
+{
+    using unordered_set = boost::intrusive::unordered_set<Foo>;
+
+    std::vector<Foo> values;
+    for (int i = 0; i < 100; ++i)
+    {
+        if (i % 2 == 0)
+        {
+            values.push_back(i);
+        }
+    }
+
+    // unordered set and map containers require supplementary memory
+    // pool to be given (which is also called buckets)
+    using bucket = unordered_set::bucket_type;
+    std::vector<bucket> buckets;
+    buckets.resize(20);
+
+    unordered_set set(unordered_set::bucket_traits(buckets.data(), buckets.size()));
+
+    for (auto& foo : values)
+    {
+        set.insert(foo);
+    }
+
+    const auto lookup = [&set](int value) {
+        std::cout << value << " ";
+        if (set.count(Foo(value)))
+        {
+            std::cout << "is in the set";
+        }
+        else
+        {
+            std::cout << "is not in the set";
+        }
+        std::cout << std::endl;
+    };
+
+    lookup(4);
+    lookup(5);
 }
 
 RUN_DEMOS
