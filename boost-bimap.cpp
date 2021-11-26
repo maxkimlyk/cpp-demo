eeb4cedb60d73e1e5371f04905d1db5610450a43
Max Kimlyk <maxskater3@yandex.ru>
2021-11-26 12:03:56 +0300

    bimap

---
+++ boost-bimap.cpp
@@ -0,0 +1,19 @@
+#include "demo-common.h"
+
+#include <string>
+
+#include <boost/bimap/bimap.hpp>
+
+DEMO(bimap)
+{
+    using Bimap = boost::bimaps::bimap<int, std::string>;
+    Bimap bimap;
+
+    bimap.insert(Bimap::value_type(1, "one"));
+    bimap.insert(Bimap::value_type(5, "five"));
+
+    std::cout << "pair for 1 is " << bimap.left.find(1)->second << std::endl;
+    std::cout << "pair for 'five' is " << bimap.right.find("five")->second << std::endl;
+}
+
+RUN_DEMOS
