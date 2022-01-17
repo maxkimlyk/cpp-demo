8088d73528357e15d787f4410147f17feb8cbe66
Max Kimlyk <maxskater3@yandex.ru>
2022-01-17 10:30:14 +0300

    fusion 2

--- boost-fusion.cpp
+++ boost-fusion.cpp
@@ -2,6 +2,8 @@
 #include <boost/fusion/include/algorithm.hpp>
 #include <boost/fusion/include/pair.hpp>
 #include <boost/fusion/include/map.hpp>
+#include <boost/fusion/include/set.hpp>
+#include <boost/fusion/include/std_tuple.hpp>
 
 #include "demo-common.h"
 
@@ -28,12 +30,14 @@ DEMO(for_each)
     boost::fusion::for_each(boost::fusion::filter_if<boost::is_class<_>>(v), print_with_type);
 }
 
+// clang-format off
 namespace fields
 {
-struct key {};
+struct key{};
 struct name {};
 struct value {};
 } // namespace fields
+// clang-format on
 
 DEMO(map)
 {
@@ -48,13 +52,41 @@ DEMO(map)
     auto serialize = [](const auto& map) {
         std::cout << "{";
         boost::fusion::for_each(map, [](const auto& pair) {
-          using Pair = std::decay_t<decltype(pair)>;
-          using First = typename Pair::first_type;
-          std::cout << typeid(First).name() << ": " << pair.second << ", "; });
+            using Pair = std::decay_t<decltype(pair)>;
+            using First = typename Pair::first_type;
+            std::cout << typeid(First).name() << ": " << pair.second << ", ";
+        });
         std::cout << "}" << std::endl;
     };
 
     serialize(obj);
+}
+
+DEMO(cons)
+{
+    // lisp-style list (has front (car) and tail (cdr))
+
+    using boost::fusion::cons;
+
+    cons<int, cons<std::string>> list(10, cons<std::string>("ten"));
+    std::cout << boost::fusion::at_c<0>(list) << ", " << boost::fusion::at_c<1>(list) << std::endl;
+}
+
+DEMO(set)
+{
+    boost::fusion::set<int, short, float> s(1, 2, 3.5);
+    std::cout << "float: " << boost::fusion::at_key<float>(s) << std::endl;
+    std::cout << "has int?: " << std::boolalpha << boost::fusion::has_key<int>(s) << std::endl;
+}
+
+DEMO(std_tuple_adapter)
+{
+    const auto print_with_type = [](const auto& value) {
+        std::cout << typeid(value).name() << ": " << value << std::endl;
+    };
+
+    auto tpl = std::make_tuple(0, '0', "zero");
+    boost::fusion::for_each(tpl, print_with_type);
 }
 
 RUN_DEMOS
